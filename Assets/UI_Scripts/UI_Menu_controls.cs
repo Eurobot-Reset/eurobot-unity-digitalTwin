a1f031324f65f8c5cec0bb955e68c89b5e0f5387
Kate-Sk <Ekaterina.Karmanova@skoltech.ru>
2020-05-20 03:00:30 +0300

    Added new Scene for Menu

---
+++ Assets/UI_Scripts/UI_Menu_controls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_controls : MonoBehaviour
+{
+
+
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/UI_Scripts/UI_Settings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Settings : MonoBehaviour
+{
+    private bool isFullScreen;
+    public void FullScreenToggle()
+    {
+        isFullScreen = ! isFullScreen;
+        Screen.fullScreen = isFullScreen;
+    }
+}
