abeb7538c6bdbe923a591a679da84013cf277c72
Pavel Kopanev <kopanev-pavel-spb@yandex.ru>
2020-05-25 16:47:08 +0300

    Constraints added, rigidbody params fixed

---
+++ Assets/Scripts/RobotControl/RigidbodySettings/ConstraintsRobot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstraintsRobot : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //This locks the RigidBody so that it does not move or rotate in the some axis.
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
+    }
+}
---
+++ Assets/Scripts/RobotControl/RigidbodySettings/ConstraintsTable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstraintsTable : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //This locks the RigidBody so that it does not move or rotate in the some axis.
+        rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationY | 
+        RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotationX |
+        RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
+    }
+}
+
+
