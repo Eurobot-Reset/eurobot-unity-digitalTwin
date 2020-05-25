62c50a35ae6de3e894bd60c6da3f111db733e4da
Pavel Kopanev <kopanev-pavel-spb@yandex.ru>
2020-05-25 16:01:41 +0300

    Rigidbody fixes

---
+++ Assets/Scripts/RobotControl/RigidbodySettings/CenterOfMass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterOfMass : MonoBehaviour
+{
+    public Vector3 com;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = com;
+    }
+}
