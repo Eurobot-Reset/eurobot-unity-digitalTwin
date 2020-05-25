720b6afcb8bea1349af38819c72775df074ae8ae
Pavel Kopanev <kopanev-pavel-spb@yandex.ru>
2020-05-25 16:25:31 +0300

    Rigidbody fixes

---
+++ Assets/Scripts/RobotControl/RigidbodySettings/RigidbodySettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Expose center of mass to allow it to be set from the inspector
+// Expose tensor of inertia to allow adjustment from the inspector
+// Resets the inertia tensor to be the coordinate system of the transform
+public class RigidbodySettings : MonoBehaviour
+{
+    public Vector3 centerOfMass;
+    public Vector3 inertiaTensor;
+    public Quaternion inertiaTensorRotation;
+    
+    public Rigidbody rb;
+
+    void Start()
+    {   
+        inertiaTensorRotation = Quaternion.identity;
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass;
+        rb.inertiaTensor = inertiaTensor;
+        rb.inertiaTensorRotation = inertiaTensorRotation;
+    }
+}
