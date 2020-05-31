3c8920a00c45ddf845f484759b3c314edd58ccd3
Steplerchik <steplerchik@gmail.com>
2020-05-31 22:44:53 +0300

    Scores are calculated in multiplayer but for 1 side only

--- Assets/Scripts/Score_calc/Score_calc.cs
+++ Assets/Scripts/Score_calc/Score_calc.cs
@@ -5,10 +5,6 @@ using UnityEngine.UI;
 
 public class Score_calc : MonoBehaviour
 {
-    // private Text ScoreText;
-    // private Text RedCupsText;
-    // private Text GreenCupsText;
-    // private Text WindsocksText;
     private int Score;
 
     public Text ScoreText;
@@ -16,12 +12,21 @@ public class Score_calc : MonoBehaviour
     public Text GreenCupsText;
     public Text WindsocksText;
     public Text LighthouseText;
+
+    public Text ScoreText_racer;
+    public Text RedCupsText_racer;
+    public Text GreenCupsText_racer;
+    public Text WindsocksText_racer;
+    public Text LighthouseText_racer;
+
+    public Text ScoreText_fatboy;
+    public Text RedCupsText_fatboy;
+    public Text GreenCupsText_fatboy;
+    public Text WindsocksText_fatboy;
+    public Text LighthouseText_fatboy;
     
     public string side_color;
-    //public bool racer = GameMode.robot[0];
-    //public bool fatboy = GameMode.robot[1];
-    //public bool racer_color_is_yellow = GameMode.isYellowSide[0];
-    //public bool fatboy_color_is_yellow = GameMode.isYellowSide[1];
+
     public static bool racer;
     public static bool fatboy;
     public static bool racer_color_is_yellow;
@@ -57,17 +62,39 @@ public class Score_calc : MonoBehaviour
     private string weathervaneStatus;
 
     public int lighthouse_score = 0;
+
+    private bool OneSideOnly = true;
     
     // Start is called before the first frame update
     void Start()
     {
-       // ScoreText = GameObject.Find("Text_Score").GetComponent<Text>();
-       // RedCupsText = GameObject.Find("Text_Red_Cups").GetComponent<Text>();
-       // GreenCupsText = GameObject.Find("Text_Green_Cups").GetComponent<Text>();
-       // WindsocksText = GameObject.Find("Text_windsock").GetComponent<Text>();
         lighthouse_yellow = GameObject.Find("Light_House");
         lighthouse_blue = GameObject.Find("Light_House1");
 
+        if ((racer) && (fatboy))
+        {
+            if (((racer_color_is_yellow) && (fatboy_color_is_yellow)) || ((!(racer_color_is_yellow)) && (!(fatboy_color_is_yellow))))
+                OneSideOnly = true;
+            else
+                OneSideOnly = false;
+        }
+        else OneSideOnly = true;
+
+        if (OneSideOnly)
+            OneSideSetup();
+        //else
+        //    TwoSidesSetup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OneSideOnly)
+            OneSideUpdate();
+    }
+
+    void OneSideSetup()
+    {
         if (racer)
         {
             if (racer_color_is_yellow)
@@ -119,8 +146,12 @@ public class Score_calc : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    //void TwoSidesSetup()
+    //{
+    //
+    //}
+
+    void OneSideUpdate()
     {
         // Cups:
         SmallPortCups = SmallPort.GetComponent<All_cup_detection>().CupsInsideCount;
@@ -155,38 +186,21 @@ public class Score_calc : MonoBehaviour
         if (side_color == "Blue")
         {
             if ((windsocksStatus[0]) && (windsocksStatus[1]))
-            {
                 windsocks_score = 15;
-                WindsocksText.text = "2";
-            }
             else if ((windsocksStatus[0]) || (windsocksStatus[1]))
-            {
                 windsocks_score = 5;
-                WindsocksText.text = "1";
-            }
             else
-            {
                 windsocks_score = 0;
-                WindsocksText.text = "0";
-            }
         }
         else if (side_color == "Yellow")
         {
             if ((windsocksStatus[2]) && (windsocksStatus[3]))
-            {
                 windsocks_score = 15;
-                WindsocksText.text = "2";
-            }
+
             else if ((windsocksStatus[2]) || (windsocksStatus[3]))
-            {
                 windsocks_score = 5;
-                WindsocksText.text = "1";
-            }
             else
-            {
                 windsocks_score = 0;
-                WindsocksText.text = "0";
-            }
         }
 
         // Mooring zone:
@@ -194,7 +208,7 @@ public class Score_calc : MonoBehaviour
         insideSouth = SouthZone.GetComponent<Mooring_zone_detector>().RobotInside;
         weathervaneStatus = tableStateHandler.weathervaneStatus;
 
-        if (racer && fatboy)
+        if ((racer) && (fatboy))
         {
             if ((insideNorth != 0) && (insideSouth != 0))
             {
@@ -216,7 +230,7 @@ public class Score_calc : MonoBehaviour
                     {
                         mooring_score = 0;
                     }
-                    
+
                 }
                 else
                 {
@@ -245,39 +259,59 @@ public class Score_calc : MonoBehaviour
             {
                 mooring_score = insideSouth * 10 + insideNorth * 5;
             }
-            
+
         }
 
         //Lighthouse:
         if (lighthouse_yellow != null)
         {
             if (lighthouse_yellow.GetComponent<LightHouseScript>().lightHouseIsActivated)
-            {
                 lighthouse_score = 15;
-                LighthouseText.text = "1";
-            }
-            else LighthouseText.text = "0";
         }
-
         if (lighthouse_blue != null)
         {
             if (lighthouse_blue.GetComponent<LightHouseScript1>().lightHouseIsActivated)
-            {
                 lighthouse_score = 15;
-                LighthouseText.text = "1";
-            }
-            else LighthouseText.text = "0";
         }
 
-
+        // Sum score calculation
         Score = (SmallPortGreenCups + SmallPortRedCups) * 2 + SmallPortPairCups * 2 + (SmallPortCups - SmallPortGreenCups - SmallPortRedCups) * 1 + (BigPortGreenCups + BigPortRedCups) * 2 + BigPortPairCups * 2 + (BigPortCups - BigPortGreenCups - BigPortRedCups) * 1 + windsocks_score + mooring_score + lighthouse_score;
-        ScoreText.text = "Score: " + Score.ToString();
-
-        RedCupsText.text = (SmallPortRedCups + BigPortRedCups).ToString();
-        GreenCupsText.text = (SmallPortGreenCups + BigPortGreenCups).ToString();
-
         
-
-
+        // Text representation
+        if ((racer) && (fatboy))
+        {
+            if (windsocks_score == 15)
+            { WindsocksText_racer.text = "2"; WindsocksText_fatboy.text = "2"; }
+            else if (windsocks_score == 5)
+            { WindsocksText_racer.text = "1"; WindsocksText_fatboy.text = "1"; }
+            else if (windsocks_score == 0)
+            { WindsocksText_racer.text = "0"; WindsocksText_fatboy.text = "0"; }
+            if (lighthouse_score == 15)
+            { LighthouseText_racer.text = "1"; LighthouseText_fatboy.text = "1"; }
+            else
+            { LighthouseText_racer.text = "0"; LighthouseText_fatboy.text = "0"; }
+            RedCupsText_racer.text = (SmallPortRedCups + BigPortRedCups).ToString();
+            RedCupsText_fatboy.text = (SmallPortRedCups + BigPortRedCups).ToString();
+            GreenCupsText_racer.text = (SmallPortGreenCups + BigPortGreenCups).ToString();
+            GreenCupsText_fatboy.text = (SmallPortGreenCups + BigPortGreenCups).ToString();
+            ScoreText_racer.text = "Score: " + Score.ToString();
+            ScoreText_fatboy.text = "Score: " + Score.ToString();
+        }
+        else
+        {
+            if (windsocks_score == 15)
+                WindsocksText.text = "2";
+            else if (windsocks_score == 5)
+                WindsocksText.text = "1";
+            else if (windsocks_score == 0)
+                WindsocksText.text = "0";
+            if (lighthouse_score == 15)
+                LighthouseText.text = "1";
+            else
+                LighthouseText.text = "0";
+            RedCupsText.text = (SmallPortRedCups + BigPortRedCups).ToString();
+            GreenCupsText.text = (SmallPortGreenCups + BigPortGreenCups).ToString();
+            ScoreText.text = "Score: " + Score.ToString();
+        }
     }
 }
