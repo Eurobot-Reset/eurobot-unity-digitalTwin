d61562f4b85fc9fb2a274819a061c6d7e0df548b
Kate-Sk <Ekaterina.Karmanova@skoltech.ru>
2020-05-31 15:38:33 +0300

    4 cameras for Racer and Fatboy added

--- Assets/UI_Scripts/Camera_game_chose.cs
+++ Assets/UI_Scripts/Camera_game_chose.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.XR;
 using UnityEngine;
 
 public class Camera_game_chose : MonoBehaviour
 {
+    public GameObject camera_G2_racer;
+    public GameObject camera_G2_fatboy;
+    public GameObject camera_G2_racer_gripper;
+    public GameObject camera_G2_fatboy_gripper;
+
+    //public int a = 0;
+    //public int b = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,32 @@ public class Camera_game_chose : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameMode.Game_2_active)
+        {
+            if (Input.GetKey(KeyCode.RightShift))
+            {
+                camera_G2_fatboy.SetActive(true);
+                camera_G2_fatboy_gripper.SetActive(false);
+            }
+            if (Input.GetKey(KeyCode.RightControl))
+            {
+                    camera_G2_fatboy.SetActive(false);
+                    camera_G2_fatboy_gripper.SetActive(true);
+            }
+ 
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                camera_G2_racer.SetActive(true);
+                camera_G2_racer_gripper.SetActive(false);                   
+            }
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                camera_G2_racer.SetActive(false);
+                camera_G2_racer_gripper.SetActive(true);
+            }
+          
+        }
     }
+  
 }
+
--- Assets/UI_Scripts/UI_mode_2.cs
+++ Assets/UI_Scripts/UI_mode_2.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.XR;
 using UnityEngine;
 
 public class UI_mode_2 : MonoBehaviour
@@ -8,12 +9,23 @@ public class UI_mode_2 : MonoBehaviour
     public GameObject camera_G1_fatboy;
     public GameObject camera_G2_racer;
     public GameObject camera_G2_fatboy;
+    public GameObject camera_G2_racer_gripper_1;
+    public GameObject camera_G2_racer_gripper_2;
+    public GameObject camera_G2_racer_gripper_3;
+    
+    public GameObject camera_G2_fatboy_gripper_1;
+    public GameObject camera_G2_fatboy_gripper_2;
+    public GameObject camera_G2_fatboy_gripper_3;
+    
     public GameObject camera_G2_up;
     public GameObject game1_ui;
     public GameObject game2_ui;
+
+    public int a = 1;
+    public int b = 1;
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
@@ -21,14 +33,14 @@ public class UI_mode_2 : MonoBehaviour
     {
         if (GameMode.Game_2_active)
         {
+            Game2_CAMERAS_switch(true);
             Game1_CAMERAS(false);
-            Game2_CAMERAS(true);
             game1_ui.SetActive(false);
             game2_ui.SetActive(true);
         }
         else
         {
-            Game1_CAMERAS(true); 
+            Game1_CAMERAS(true);
             Game2_CAMERAS(false);
             game1_ui.SetActive(true);
             game2_ui.SetActive(false);
@@ -37,13 +49,111 @@ public class UI_mode_2 : MonoBehaviour
 
     void Game2_CAMERAS(bool active)
     {
+        camera_G2_fatboy_gripper_1.SetActive(active);
+        camera_G2_fatboy_gripper_2.SetActive(active);
+        camera_G2_fatboy_gripper_3.SetActive(active);
         camera_G2_fatboy.SetActive(active);
+
+        camera_G2_racer_gripper_1.SetActive(active);
+        camera_G2_racer_gripper_2.SetActive(active);
+        camera_G2_racer_gripper_3.SetActive(active);
         camera_G2_racer.SetActive(active);
-        camera_G2_up.SetActive(active);
+        camera_G2_up.SetActive(active);             
     }
+
     void Game1_CAMERAS(bool active)
     {
         camera_G1_fatboy.SetActive(active);
         camera_G1_racer.SetActive(active);
     }
+
+    void Game2_CAMERAS_switch(bool active)
+    {
+        if (active)
+        {
+            if (Input.GetKeyDown(KeyCode.RightShift)) a +=1;
+            if (Input.GetKeyDown(KeyCode.RightControl)) a -=1;
+
+            if (a > 4) a = 1;
+            if (a < 1) a = 4;
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))  b +=1;
+            if (Input.GetKeyDown(KeyCode.LeftControl)) b -=1;
+
+            if (b > 4) b = 1;
+            if (b < 1) b = 4;
+            
+            Camera_fatboy_change(a);
+            Camera_racer_change(b);
+        }
+    }
+
+    void Camera_fatboy_change(int a)
+    {
+        switch (a)
+        {
+            case 1:
+            camera_G2_fatboy.SetActive(true);
+            camera_G2_fatboy_gripper_1.SetActive(false);
+            camera_G2_fatboy_gripper_2.SetActive(false);
+            camera_G2_fatboy_gripper_3.SetActive(false); 
+            break;
+
+            case 2:
+                camera_G2_fatboy.SetActive(false);
+                camera_G2_fatboy_gripper_1.SetActive(true);
+                camera_G2_fatboy_gripper_2.SetActive(false);
+                camera_G2_fatboy_gripper_3.SetActive(false);
+                break;
+
+            case 3:
+                camera_G2_fatboy.SetActive(false);
+                camera_G2_fatboy_gripper_1.SetActive(false);
+                camera_G2_fatboy_gripper_2.SetActive(true);
+                camera_G2_fatboy_gripper_3.SetActive(false);
+                break;
+
+            case 4:
+                camera_G2_fatboy.SetActive(false);
+                camera_G2_fatboy_gripper_1.SetActive(false);
+                camera_G2_fatboy_gripper_2.SetActive(false);
+                camera_G2_fatboy_gripper_3.SetActive(true);
+                break;
+        }
+    }
+        
+    void Camera_racer_change(int b)
+    {
+        switch (b)
+        {
+            case 1:
+                camera_G2_racer.SetActive(true);
+                camera_G2_racer_gripper_1.SetActive(false);
+                camera_G2_racer_gripper_2.SetActive(false);
+                camera_G2_racer_gripper_3.SetActive(false);
+                break;
+
+            case 2:
+                camera_G2_racer.SetActive(false);
+                camera_G2_racer_gripper_1.SetActive(true);
+                camera_G2_racer_gripper_2.SetActive(false);
+                camera_G2_racer_gripper_3.SetActive(false);
+                break;
+
+            case 3:
+                camera_G2_racer.SetActive(false);
+                camera_G2_racer_gripper_1.SetActive(false);
+                camera_G2_racer_gripper_2.SetActive(true);
+                camera_G2_racer_gripper_3.SetActive(false);
+                break;
+
+            case 4:
+                camera_G2_racer.SetActive(false);
+                camera_G2_racer_gripper_1.SetActive(false);
+                camera_G2_racer_gripper_2.SetActive(false);
+                camera_G2_racer_gripper_3.SetActive(true);
+                break;
+        }
+    }   
+
 }
